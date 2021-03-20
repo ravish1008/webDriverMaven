194bf06be02aadeb76ee253a64c20607e4218748
ravish <ravish.chauhan1008@gmail.com>
2021-03-20 23:20:38 +0530

    init commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+	}
+	@Test
+ public void doLogin() {
+	 driver.get("http://gmail.com");
+	 
+	 driver.findElement(By.name("identifier")).sendKeys("kumr87121@gmail.com");
+	 driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/div[2]")).click();
+	 driver.findElement(By.tagName("input")).sendKeys("xxxxx");
+ }
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
